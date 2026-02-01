6d2c7dff8236cfda2d6edfda85a62739fe63ede6
Aleksey Kravchenko <const346@gmail.com>
2026-02-01 15:41:26 +0100

    Add UnpairedRoomGenerator

--- Assets/Game/Scripts/Generators/UnpairedRoomGenerator.cs
+++ Assets/Game/Scripts/Generators/UnpairedRoomGenerator.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -9,15 +8,113 @@ public class UnpairedRoomGenerator : MonoBehaviour
     [SerializeField] private Actor _actorTemplate;
     [SerializeField] private Transform _actorSpawnsContainer;
 
+    private Room _room;
+
     private void Awake()
     {
-        var room = GetComponent<Room>();
-        room.OnGenerate.AddListener(Generate);
+        _room = GetComponent<Room>();
+        _room.OnGenerate.AddListener(Generate);
+    }
+
+    private void SetCategoryValue(ref ActorMask mask, string category, string value)
+    {
+        if (category == "Horn")
+        {
+            mask.Horn = value;
+        }
+        if (category == "Color")
+        {
+            mask.Color = value;
+        }
+        if (category == "Type")
+        {
+            mask.Type = value;
+        }
     }
 
     private void Generate()
     {
-        Debug.Log("UnpairedRoomGenerator Generate called");
+        ClearActors();
+
+        var table = new Dictionary<string, string[]>()
+        {
+            { "Horn", new[] { "Deer", "Goat", "Ram" } },
+            { "Color", new[] { "White", "Black", "Red" } },
+            { "Type", new[] { "TypeA", "TypeB", "TypeC" } },
+        };
+
+        var categories = table.Keys.ToArray().OrderBy(_ => UnityEngine.Random.value).ToArray();
+        var category1 = categories[0]; // 1 из 3
+        var category2 = categories[1]; // 1 из 2
+        var category3 = categories[2]; // 1 из 2
+
+        table["Horn"] = table["Horn"].OrderBy(_ => UnityEngine.Random.value).ToArray();
+        table["Color"] = table["Color"].OrderBy(_ => UnityEngine.Random.value).ToArray();
+        table["Type"] = table["Type"].OrderBy(_ => UnityEngine.Random.value).ToArray();
+
+        var masks = new List<ActorMask>();
+        for (int i = 0; i < 3; i++)
+        {
+            var mask1 = new ActorMask
+            {
+                HasGold = false,
+                HasPattern = false,
+            };
+            SetCategoryValue(ref mask1, category1, table[category1][i]);
+            SetCategoryValue(ref mask1, category2, table[category2][0]);
+            SetCategoryValue(ref mask1, category3, table[category3][0]);
+
+            var mask2 = new ActorMask
+            {
+                HasGold = false,
+                HasPattern = false,
+            };
+
+            SetCategoryValue(ref mask2, category1, table[category1][i]);
+            SetCategoryValue(ref mask2, category2, table[category2][1]);
+            SetCategoryValue(ref mask2, category3, table[category3][1]);
+
+            masks.Add(mask1);
+            masks.Add(mask2);
+        }
+
+        var answer = new ActorMask
+        {
+            HasGold = false,
+            HasPattern = false,
+        };
+
+        SetCategoryValue(ref answer, category1, table[category1][UnityEngine.Random.Range(0, table[category1].Length)]);
+        SetCategoryValue(ref answer, category2, table[category2][0]);
+        SetCategoryValue(ref answer, category3, table[category3][1]);
+
+        masks.Add(answer);
+
+        var actorSpawns = _actorSpawnsContainer.GetComponentsInChildren<Transform>();
+        actorSpawns = actorSpawns.Where(x => x != _actorSpawnsContainer)
+            .OrderBy(_ => Random.value).ToArray();
+
+        for (int i = 0; i < masks.Count; i++)
+        {
+            var actor = Instantiate(_actorTemplate, actorSpawns[i].position, actorSpawns[i].rotation, transform);
+            actor.ApplyCustomize(masks[i]);
+
+            if (i == masks.Count - 1)
+            {
+                _room.CorrectActor = actor;
+            }
+
+            Debug.Log($"Actor {i}: Type={masks[i].Type}, Color={masks[i].Color}, Horn={masks[i].Horn}, IsCorrect={i == masks.Count - 1}");
+        }
+    }
+
+    private void ClearActors()
+    {
+        var existingActors = GetComponentsInChildren<Actor>();
+        foreach (var actor in existingActors)
+        {
+            Destroy(actor.gameObject);
+        }
     }
 
     private void OnDrawGizmos()
